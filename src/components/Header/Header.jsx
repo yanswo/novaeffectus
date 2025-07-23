b3b22be9b45727d57b0fa24ee752969ee726a3c7
yanswo <yanlucas2202@gmail.com>
2025-07-23 12:48:25 -0300

    melhorias em mainbanner, header responsivo.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 
 function Header({ theme }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className={`site-header ${theme}`}>
+    <header className={`site-header ${theme} ${isMenuOpen ? "menu-open" : ""}`}>
       <div className="container header-wrapper">
         <NavLink to="/" className="logo">
           NovaEffectus
         </NavLink>
-        <nav className="main-nav">
-          <ul>
-            <li>
-              {}
-              <a href="/#servicos" className="nav-link">
-                Serviços
-              </a>
-            </li>
-            <li>
-              <NavLink to="/sobre" className="nav-link">
-                Sobre
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/suporte" className="nav-link">
-                Suporte
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+
         <div className="header-actions">
+          <nav className="main-nav">
+            <ul>
+              <li>
+                <a
+                  href="/#servicos"
+                  className="nav-link"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Serviços
+                </a>
+              </li>
+              <li>
+                <NavLink
+                  to="/sobre"
+                  className="nav-link"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Sobre
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/suporte"
+                  className="nav-link"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Suporte
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
           <button className="lang-switcher">PT</button>
           <a href="#" className="btn btn-white">
             Baixe o App
           </a>
-          <button className="hamburger-menu">
-            <span></span>
-            <span></span>
-            <span></span>
+
+          <button
+            className="hamburger-menu"
+            onClick={toggleMenu}
+            aria-label={isMenuOpen ? "Fechar menu" : "Abrir menu"}
+          >
             <span></span>
             <span></span>
             <span></span>
